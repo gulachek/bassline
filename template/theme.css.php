11e77932a97ff9428af2c1b52df8ba31aaa7624c
Nicholas Gulachek <gulac004@umn.edu>
2023-04-15 12:33:40 -0500

    sys-theme -> system-theme

--- template/theme.css.php
+++ template/theme.css.php
@@ -2,7 +2,7 @@
 	color-scheme: light dark;
 
 <?php foreach ($LIGHT_SYS as $name => $color): ?>
-	--sys-theme-<?=$name?>: <?=$color?>;
+	--system-theme-<?=$name?>: <?=$color?>;
 <?php endforeach; ?>
 
 <?php foreach ($LIGHT_APP as $name => $color): ?>
@@ -13,7 +13,7 @@
 @media (prefers-color-scheme: dark) {
 	:root {
 <?php foreach ($DARK_SYS as $name => $color): ?>
-		--sys-theme-<?=$name?>: <?=$color?>;
+		--system-theme-<?=$name?>: <?=$color?>;
 <?php endforeach; ?>
 
 <?php foreach ($DARK_APP as $name => $color): ?>
