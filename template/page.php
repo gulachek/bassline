87af3841b3763ba4fc90c513445a4d8acb4d50f8
Nicholas Gulachek <gulac004@umn.edu>
2023-04-22 13:24:29 -0500

    fix styling issue on login button

--- template/page.php
+++ template/page.php
@@ -34,7 +34,9 @@
 		</div>
 	</div>
 <?php else: ?>
-	<a href="/login/" class="login"> Log in </a>
+	<div class="right">
+		<a href="/login/" class="login"> Log in </a>
+	</div>
 <?php endif; ?>
 
 </nav>
