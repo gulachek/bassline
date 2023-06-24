33a411d1c0cb862c4bc31d75dae781044294a65f
Nicholas Gulachek <gulac004@umn.edu>
2023-06-24 11:55:52 -0500

    auto formatting

--- bin/serve.php
+++ bin/serve.php
@@ -1,18 +1,14 @@
 <?php
 
 # Do this for better performance
-if (isset($_SERVER['AUTOLOAD_PATH']))
-{
+if (isset($_SERVER['AUTOLOAD_PATH'])) {
 	require_once $_SERVER['AUTOLOAD_PATH'];
-}
-else # nicer for development
+} else # nicer for development
 {
 	$dir = __DIR__;
-	while (!file_exists("$dir/vendor/autoload.php"))
-	{
+	while (!file_exists("$dir/vendor/autoload.php")) {
 		$next = dirname($dir);
-		if ($next == $dir)
-		{
+		if ($next == $dir) {
 			throw new \Exception("vendor/autoload.php not found in ancestor directory");
 		}
 		$dir = $next;
@@ -25,8 +21,7 @@ else # nicer for development
 header('Referrer-Policy: no-referrer-when-downgrade');
 // TODO: remove inline scripts from csp
 
-if (!isset($_SERVER['SITE_CONFIG_PATH']))
-{
+if (!isset($_SERVER['SITE_CONFIG_PATH'])) {
 	$_SERVER['SITE_CONFIG_PATH'] = getenv('SITE_CONFIG_PATH');
 }
 
@@ -35,7 +30,6 @@ $server = new \Gulachek\Bassline\Server();
 // TODO: add a way to generate static content config files and
 // opt out of this check via $_SERVER variable to go straight to
 // render()
-if (!$server->serveStaticContent())
-{
+if (!$server->serveStaticContent()) {
 	$server->render();
 }
